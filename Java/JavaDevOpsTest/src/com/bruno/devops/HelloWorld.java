d192bebbc4caf37524a27da0f1391428db0b750f
demilbr1 <19268730+demilbr1@users.noreply.github.com>
2023-04-01 14:34:54 +0200

    First commit

---
+++ Java/JavaDevOpsTest/src/com/bruno/devops/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.bruno.devops;
+/**
+ * 
+ */
+
+/**
+ * @author bruno
+ *
+ */
+class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
